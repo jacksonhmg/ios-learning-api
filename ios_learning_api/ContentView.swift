b92b9ede87e03e0659e194ecf0062516439839b4
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-20 14:49:48 -0700

    init navigation, fetch Url safely, decode JSON in course structure

--- ios_learning_api/ContentView.swift
+++ ios_learning_api/ContentView.swift
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
+struct Course: Hashable, Codable {
+    let name: String
+    let image: String
+}
+
+class ViewModel: ObservableObject {
+    @Published var courses: [Course] = []
+    
+    func fetch () {
+        guard let url = URL(string: "https://iosacademy.io/api/v1/courses/index.php") else{ return }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            // Convert to JSON
+            do {
+                let courses = try JSONDecoder().decode([Course].self, from: data)
+                DispatchQueue.main.async {
+                    self?.courses = courses
+                }
+            }
+            catch {
+                print(error)
+            }
+        }
+        
+        task.resume()
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            List {
+                
+            }.navigationTitle("Courses")
         }
-        .padding()
     }
 }
 
