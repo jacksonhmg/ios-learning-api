a5e530dc7e2928933afd3a52ec1d90ac56ec0531
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-20 14:59:35 -0700

    fetch names correctly

--- ios_learning_api/ContentView.swift
+++ ios_learning_api/ContentView.swift
@@ -40,11 +40,25 @@ class ViewModel: ObservableObject {
 }
 
 struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
     var body: some View {
         NavigationView{
             List {
-                
-            }.navigationTitle("Courses")
+                ForEach(viewModel.courses, id: \.self) { course in
+                    HStack {
+                        Image("")
+                            .frame(width: 130, height: 70)
+                            .background(Color.gray)
+                        Text(course.name)
+                            .bold()
+                    }
+                    .padding(3)
+                }
+            }
+            .navigationTitle("Courses")
+            .onAppear {
+                viewModel.fetch()
+            }
         }
     }
 }
