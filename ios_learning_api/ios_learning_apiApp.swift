297da2c5bd1969d9228fba564ec94ec7d9df0825
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-20 14:27:10 -0700

    Initial Commit

---
+++ ios_learning_api/ios_learning_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_learning_apiApp.swift
+//  ios_learning_api
+//
+//  Created by jackson mowatt gok on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ios_learning_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
